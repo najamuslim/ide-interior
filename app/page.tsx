8686f4d498d573901f8f1cb4447245093856f52b
Najatul Muslim Dinatra <najatulmuslimdinatra@gmail.com>
2024-11-09 19:57:57 +0700

    update wording

--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,20 @@ export default function HomePage() {
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Ciptakan ruang impian Anda {" "}
+          Desain Ruang Impian Anda dengan {" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
-            <span className="relative">dengan AI</span>
-          </span>{" "}
-          untuk Semua.
+            <span className="relative">IDEInterior</span>
+          </span>
         </h1>
         <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
-        Unggah foto ruangan Anda dan lihat transformasinya dalam berbagai tema. Mulai desain ulang ruangan Anda sekarang!
+        Unggah foto ruang Anda, dan lihat transformasinya dalam berbagai tema. Desain ulang ruangan Anda kini lebih mudah!
         </h2>
         <Link
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Desain ruang impian Anda
+          Mulai Sekarang!
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
