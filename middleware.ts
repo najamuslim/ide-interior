62408def11f59ad9a56886a4f346f1372ffdee2d
Najatul Muslim Dinatra <najatulmuslimdinatra@gmail.com>
2025-03-03 14:42:52 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Skip Next.js internals and all static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Only run on specific API routes, excluding midtrans-webhook
+    "/api/((?!midtrans-webhook).*)",
   ],
 };
