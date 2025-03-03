b4cf9bf55ac0b9813a2df26d626ac072327ae026
Najatul Muslim Dinatra <najatulmuslimdinatra@gmail.com>
2025-03-03 14:11:30 +0700

    debug webhook

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["upcdn.io", "replicate.delivery"],
   },
+  trailingSlash: false,
+  async headers() {
+    return [
+      {
+        source: "/api/midtrans-webhook",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST, OPTIONS",
+          },
+        ],
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
